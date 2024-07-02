02aee9687778dbb86640da236fdc2d854cc5c554
kemboi590 <bkemboi590@gmail.com>
2024-07-02 17:04:50 +0300

    added update entity generics

--- src/generics/generic.Controller.ts
+++ src/generics/generic.Controller.ts
@@ -5,7 +5,7 @@ export const getEntitiesController = <T>(service: (c: Context) => Promise<T[]>) 
     try {
         const entities = await service(c);
         if (entities == null || entities.length == 0) {
-            return c.text("No entities found", 404);
+            return c.text("Not found", 404);
         }
         return c.json(entities, 200);
     } catch (error: any) {
@@ -13,5 +13,46 @@ export const getEntitiesController = <T>(service: (c: Context) => Promise<T[]>) 
     }
 }
 
+// controller to get entity by id
+export const getEntityByIdController = <T>(service: (id: number, c: Context) => Promise<T | undefined>) => async (c: Context) => {
+    try {
+        const id = parseInt(c.req.param("id"));
+        if (isNaN(id)) return c.text("Invalid id", 400);
 
+        const entity = await service(id, c);
+        if (entity == null) {
+            return c.text("Not found", 404);
+        }
+        return c.json(entity, 200);
+    } catch (error: any) {
+        return c.json({ error: error?.message }, 500);
+    }
+}
 
+// controller to create entity
+export const createEntityController = <T>(service: (entity: T, c: Context) => Promise<string>) => async (c: Context) => {
+    try {
+        const entity = await c.req.json();
+        const res = await service(entity, c);
+        if (!res) return c.text("Not created", 400);
+        return c.json({ message: res }, 201);
+    } catch (error: any) {
+        return c.json({ error: error?.message }, 500);
+    }
+}
+
+// controller to update entity
+export const updateEntityController = <T>(service: (id: number, entity: T, c: Context) => Promise<string>) => async (c: Context) => {
+    try {
+        const id = parseInt(c.req.param("id"));
+        if (isNaN(id)) return c.text("Invalid id", 400);
+
+        // Update entity
+        const entity = await c.req.json();
+        const res = await service(id, entity, c);
+        if (!res) return c.text("Not updated", 400);
+        return c.json({ message: res }, 200);
+    } catch (error: any) {
+        return c.json({ error: error?.message }, 500);
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,71 +1,47 @@
 import { Context } from "hono";
 import { getUsersService, getUserByIdService, createUserService, updateUserService, deleteUserService } from "./users.service";
-import { getEntitiesController } from "../generics/generic.Controller";
+import { getEntitiesController, getEntityByIdController,createEntityController,updateEntityController } from "../generics/generic.Controller";
 
 // get all users
 
 export const getUsersController = getEntitiesController(getUsersService);
-// export const getUsersController = async (c: Context) => {
+// get user by id
+export const getUserByIdController = getEntityByIdController(getUserByIdService);
+// create user
+export const createUserController = createEntityController(createUserService);
+
+// export const createUserController = async (c: Context) => {
 //     try {
-//         const users = await getUsersService();
-//         if (users == null || users.length == 0) {
-//             return c.text("No users found", 404);
-//         }
-//         return c.json(users, 200);
+//         const user = await c.req.json();
+//         const newUser = await createUserService(user);
+
+//         if (!newUser) return c.text("User not created", 400);
+//         return c.json({ message: newUser }, 201);
 //     } catch (error: any) {
 //         return c.json({ error: error?.message }, 500);
 //     }
 // };
 
-// get user by id
-export const getUserByIdController = async (c: Context) => {
-    try {
-        const id = parseInt(c.req.param("id"));
-        if (isNaN(id)) {
-            return c.text("Invalid id", 400);
-        }
-        const user = await getUserByIdService(id);
-        if (user == null) {
-            return c.text("User not found", 404);
-        }
-        return c.json(user, 200);
-    } catch (error: any) {
-        return c.json({ error: error?.message }, 500);
-    }
-};
-
-// create user
-export const createUserController = async (c: Context) => {
-    try {
-        const user = await c.req.json();
-        const newUser = await createUserService(user);
-
-        if (!newUser) return c.text("User not created", 400);
-        return c.json({ message: newUser }, 201);
-    } catch (error: any) {
-        return c.json({ error: error?.message }, 500);
-    }
-};
-
 //  update user
-export const updateUserController = async (c: Context) => {
-    try {
-        // get id from url
-        const id = parseInt(c.req.param("id"));
-        if (isNaN(id)) return c.text("Invalid id", 400);
-        const user = await c.req.json();
-        //    get user by id
-        const updatedUser = await getUserByIdService(id);
-        if (!updatedUser) return c.text("User not found", 404);
+export const updateUserController = updateEntityController(updateUserService);
+// export const updateUserController = async (c: Context) => {
+//     try {
+//         // get id from url
+//         const id = parseInt(c.req.param("id"));
+//         if (isNaN(id)) return c.text("Invalid id", 400);
+//         const user = await c.req.json();
+//         //    get user by id
+//         const updatedUser = await getUserByIdService(id);
+//         if (!updatedUser) return c.text("User not found", 404);
 
-        // get data to update
-        const res = await updateUserService(id, user);
-        if (!res) return c.text("User not updated", 400);
-        return c.json({ message: res }, 200)
-    } catch (error: any) {
-        return c.json({ error: error?.message }, 500);
-    }
-};
+//         // get data to update
+//         const res = await updateUserService(id, user);
+//         if (!res) return c.text("User not updated", 400);
+//         return c.json({ message: res }, 200)
+//     } catch (error: any) {
+//         return c.json({ error: error?.message }, 500);
+//     }
+// };
 
 // delete user
 export const deleteUserController = async (c: Context) => {
