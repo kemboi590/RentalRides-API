1348ec9799795cb33d81411f30773436d2317f20
kemboi590 <bkemboi590@gmail.com>
2024-07-14 14:26:01 +0300

    deployed client to azure

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
 // export const ClientDomain = 'http://localhost:5173';
-export const ClientDomain = 'https://rental-rides-client.vercel.app';
+export const ClientDomain = 'https://happy-wave-025003603.5.azurestaticapps.net';
